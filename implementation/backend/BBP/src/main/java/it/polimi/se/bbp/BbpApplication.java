40d4c1f0695a8a308b15b42c7bfe294f9f5094ae
matteosalaa <190951201+m-tteo@users.noreply.github.com>
2025-10-22 14:59:18 +0200

    backend's boilerplate

---
+++ implementation/backend/BBP/src/main/java/it/polimi/se/bbp/BbpApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.se.bbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbpApplication.class, args);
+    }
+
+}
---
+++ implementation/backend/BBP/src/test/java/it/polimi/se/bbp/BbpApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.se.bbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
