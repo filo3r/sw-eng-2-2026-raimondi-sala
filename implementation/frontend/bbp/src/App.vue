e3ec14521883c040eae146b734fdf2edf96901fd
Filippo Raimondi <92033111+filo3r@users.noreply.github.com>
2026-01-03 18:00:21 +0100

    fix: sidebar tooltips appearing behind BikePathFinder search panel

--- implementation/frontend/bbp/src/App.vue
+++ implementation/frontend/bbp/src/App.vue
@@ -34,7 +34,7 @@ watch(() => route.path, () => {
       </div>
     </div>
 
-    <div class="drawer-side is-drawer-close:overflow-visible">
+    <div class="drawer-side is-drawer-close:overflow-visible z-50">
       <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
       <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
         <ul class="menu w-full grow">
